5fb7f1bc17867ffcd26acd5b59806bbf63fd8de6
David Gageot <david.gageot@docker.com>
2024-12-04 12:52:29 +0100

    More grids
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 04/second.rb
+++ 04/second.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/env ruby
 # Expected: 9
 
-lines = STDIN.readlines(chomp: true).map(&:chars)
+require "../common.rb"
 
-sum = 0
-(0..lines.size-3).each do |row|
-    (0..lines[0].size-3).each do |col|
-        block = lines[row][col] + lines[row][col+2] + lines[row+1][col+1] + lines[row+2][col] + lines[row+2][col+2]
-        sum +=1 if ["MSAMS", "MMASS", "SMASM", "SSAMM"].include?(block)
-    end
+grid = Grid.new(STDIN.readlines(chomp: true))
+
+count = 0
+
+(1...grid.height-1).each do |r|
+  (1...grid.width-1).each do |c|
+    next if grid[r, c] != "A"
+    corners = grid[r - 1, c - 1] + grid[r - 1, c + 1] + grid[r + 1, c + 1] + grid[r + 1, c - 1]
+    next if ["MMSS", "MSSM", "SSMM", "SMMS"].exclude?(corners)
+
+    count += 1
+  end
 end
-p sum
+
+p count
