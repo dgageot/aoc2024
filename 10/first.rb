ac2730cb24096eba5764afc73cbb7d146ac3064f
David Gageot <david.gageot@docker.com>
2024-12-10 06:44:16 +0100

    One more day
    Signed-off-by: David Gageot <david.gageot@docker.com>

---
+++ 10/first.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# Expected: 36
+
+require "../common.rb"
+
+@grid = Grid.new(STDIN.readlines(chomp: true).map { |line| line.chars.map(&:to_i) })
+
+def paths(r, c, l)
+  return [] if @grid[r, c] != l
+  return [[r, c]] if @grid[r, c] == 9
+
+  [[0, -1], [0, 1], [-1, 0], [1, 0]].flat_map { |dr, dc| paths(r + dr, c + dc, l + 1) }
+end
+
+  
+sum = 0
+@grid.select { |_, _, v| v == 0 }.each do |r, c|
+  seen = paths(r, c, 0)
+  sum += Set.new(seen).count
+end
+p sum
+
