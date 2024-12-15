b6a5c4d9c3d3233ed70426301b749b48a821cbd8
David Gageot <david.gageot@docker.com>
2024-12-15 08:37:58 +0100

    wip
    Signed-off-by: David Gageot <david.gageot@docker.com>

---
+++ 15/first.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# Expected: 12
+
+require "../common.rb"
+
+lines = STDIN.readlines(chomp: true)
+map, movements = *lines.slice_on("") 
+
+grid = Grid.new(map.map { |line| line.chars })
+puts grid
+
+movements = movements.join.chars
+p movements
+
+robot = grid.find { |_, _, v| v == "@" }[0..1]
+p robot
+
---
+++ 15/second.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# Expected: 12
+
+require "../common.rb"
+
+p STDIN.readlines(chomp: true)
