e15413e8204bba4f024372d7b4d055248a484411
David Gageot <david.gageot@docker.com>
2024-12-16 23:02:40 +0100

    wip
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 16/second.rb
+++ 16/second.rb
@@ -10,15 +10,16 @@ seen = Set.new
 pq = Heap.new { |l, r| l[0] < r[0] }
 pq << [0, grid.height - 2, 1, 0, 1]
 
+path = []
+
 loop do
     score, r, c, dr, dc = pq.pop
+    next if seen.add?([r, c]).nil?
 
     if [r, c] == [1, grid.width - 2]
         p score
         return
     end
-
-    next if seen.add?([r, c]).nil?
 
     @drdc.each do |ddr, ddc|
         next if grid[r + ddr, c + ddc] == "#"
