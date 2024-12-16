6ce8929b2a0567f1a9d5902c10ff07d5b45b5501
David Gageot <david.gageot@docker.com>
2024-12-16 19:10:21 +0100

    wip
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 15/first.rb
+++ 15/first.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# Expected: 12
+# Expected: 2028
 
 require "../common.rb"
 
@@ -14,23 +14,52 @@ lines = STDIN.readlines(chomp: true)
 map, movements = *lines.slice_on("") 
 
 grid = Grid.new(map.map { |line| line.chars })
-puts grid
+# puts grid
 
 movements = movements.join.chars
-p movements
+# p movements
 
-xr, yr = *grid.find { |_, _, v| v == "@" }[0..1]
-p yr, xr
+br, bc = *grid.find { |_, _, v| v == "@" }[0..1]
+# p br, bc
+
+# puts "Initial state:" + grid.to_s
 
 movements.each do |movement|
-    dr, dr = *@directions[movement]
+    dr, dc = *@directions[movement]
+    # puts "br=#{br + dr}, bc=#{bc + dc} grid=#{grid[br + dr, br + dc]}}"
+    if grid[br + dr, bc + dc] != "#"
+        if grid[br + dr, bc + dc] == "."
+            grid[br, bc] = "."
+            br += dr
+            bc += dc
+        else
+            count = 1
+            loop do
+                # puts "br=#{br + dr * count}, bc=#{bc + dc * count}"
+                break if grid[br + dr * count, bc + dc * count] != "O"
+                count += 1
+            end
 
-    next if grid[yr + dr, xr + dc] == "#"
-    if grid[yr + dr, xr + dc] == "."
-        yr += dr
-        xr += dc
-    else
-       # rock 
+            # puts "count=#{count}"
+
+            next if grid[br + dr * count, bc + dc * count] == "#"
+
+            grid[br, bc] = "."
+            grid[br + dr * count, bc + dc * count] = "O"
+            br += dr
+            bc += dc
+        end
     end
 
+    grid[br, bc] = "@"
+
+    # puts
+    # puts "Move #{movement}" + grid.to_s
 end
+
+sum = 0
+grid.each do |r, c, v|
+    next if grid[r, c] != "O"
+    sum += 100 * r + c
+end
+p sum
