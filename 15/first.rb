faaaf7bfca80199085f36ba6475cf606706aec7b
David Gageot <david.gageot@docker.com>
2024-12-16 19:10:54 +0100

    wip
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 15/first.rb
+++ 15/first.rb
@@ -14,19 +14,13 @@ lines = STDIN.readlines(chomp: true)
 map, movements = *lines.slice_on("") 
 
 grid = Grid.new(map.map { |line| line.chars })
-# puts grid
 
 movements = movements.join.chars
-# p movements
 
 br, bc = *grid.find { |_, _, v| v == "@" }[0..1]
-# p br, bc
-
-# puts "Initial state:" + grid.to_s
 
 movements.each do |movement|
     dr, dc = *@directions[movement]
-    # puts "br=#{br + dr}, bc=#{bc + dc} grid=#{grid[br + dr, br + dc]}}"
     if grid[br + dr, bc + dc] != "#"
         if grid[br + dr, bc + dc] == "."
             grid[br, bc] = "."
@@ -35,12 +29,9 @@ movements.each do |movement|
         else
             count = 1
             loop do
-                # puts "br=#{br + dr * count}, bc=#{bc + dc * count}"
                 break if grid[br + dr * count, bc + dc * count] != "O"
                 count += 1
             end
-
-            # puts "count=#{count}"
 
             next if grid[br + dr * count, bc + dc * count] == "#"
 
@@ -51,10 +42,7 @@ movements.each do |movement|
         end
     end
 
-    grid[br, bc] = "@"
-
-    # puts
-    # puts "Move #{movement}" + grid.to_s
+    # grid[br, bc] = "@"
 end
 
 sum = 0
