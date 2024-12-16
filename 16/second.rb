ecba80c39dd2bfece5929d2823162113b1939496
David Gageot <david.gageot@docker.com>
2024-12-16 23:01:40 +0100

    wip
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 16/second.rb
+++ 16/second.rb
@@ -1,42 +1,32 @@
 #!/usr/bin/env ruby
-# Expected: 11048
+# Expected: 7036
 
 require "../common.rb"
 require "rb_heap"
 
 grid = Grid.new(STDIN.readlines(chomp: true).map { |line| line.chars })
 
-seen = {}
+seen = Set.new
 pq = Heap.new { |l, r| l[0] < r[0] }
-pq << [0, grid.height - 2, 1, 0, 1, []]
+pq << [0, grid.height - 2, 1, 0, 1]
 
 loop do
-    score, r, c, dr, dc, path = pq.pop
-    if score.nil?
-        break
-    end
+    score, r, c, dr, dc = pq.pop
 
     if [r, c] == [1, grid.width - 2]
         p score
-        # return
+        return
     end
 
-    best = seen[[r, c]]
-    if best.nil? || best == score
-        seen[[r, c]] = score
-    else
-        next
-    end
-
-    # next if seen.add?([r, c]).nil?
+    next if seen.add?([r, c]).nil?
 
     @drdc.each do |ddr, ddc|
         next if grid[r + ddr, c + ddc] == "#"
 
         if dr == ddr && dc == ddc
-            pq << [score + 1, r + ddr, c + ddc, ddr, ddc, path + [[r, c]]]
+            pq << [score + 1, r + ddr, c + ddc, ddr, ddc]
         elsif (dr + ddr) != 0 || (dc + ddc) != 0
-            pq << [score + 1001, r + ddr, c + ddc, ddr, ddc, path + [[r, c]]]
+            pq << [score + 1001, r + ddr, c + ddc, ddr, ddc]
         end
     end
 end
