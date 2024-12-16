22a8766a725c23be3f93d6b1db01ecf604dd89c7
David Gageot <david.gageot@docker.com>
2024-12-16 21:34:02 +0100

    wip
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 16/first.rb
+++ 16/first.rb
@@ -5,10 +5,6 @@ require "../common.rb"
 require "rb_heap"
 
 grid = Grid.new(STDIN.readlines(chomp: true).map { |line| line.chars })
-# puts grid
-
-grid[grid.height-2, 1] = "."
-grid[1, grid.width-2] = "."
 
 seen = Set.new
 pq = Heap.new { |l, r| l[0] < r[0] }
@@ -16,7 +12,6 @@ pq << [0, grid.height - 2, 1, 0, 1]
 
 loop do
     score, r, c, dr, dc = pq.pop
-    # p "r #{r} c #{c} dr #{dr} dc #{dc} pq #{pq.size} score #{score}"
 
     if [r, c] == [1, grid.width - 2]
         p score
@@ -29,11 +24,8 @@ loop do
         next if grid[r + ddr, c + ddc] == "#"
 
         if dr == ddr && dc == ddc
-            # Same direction
             pq << [score + 1, r + ddr, c + ddc, ddr, ddc]
-        elsif (dr + ddr) == 0 && (dc + ddc) == 0
-        else
-            # Left or right
+        elsif (dr + ddr) != 0 || (dc + ddc) != 0
             pq << [score + 1001, r + ddr, c + ddc, ddr, ddc]
         end
     end
--- 16/second.rb
+++ 16/second.rb
@@ -1,6 +1,46 @@
 #!/usr/bin/env ruby
-# Expected: 2028
+# Expected: 64
 
 require "../common.rb"
+require "rb_heap"
 
-p STDIN.readlines(chomp: true)
+grid = Grid.new(STDIN.readlines(chomp: true).map { |line| line.chars })
+
+pq = Heap.new { |l, r| l[0] < r[0] }
+pq << [0, grid.height - 2, 1, 0, 1, Set.new()]
+
+spots = Set.new
+lowest = Float::INFINITY
+
+loop do
+    score, r, c, dr, dc, path = pq.pop
+    if score.nil?
+        break
+    end
+
+    if [r, c] == [1, grid.width - 2]
+        if lowest == Float::INFINITY
+            lowest = score
+            spots += path
+        elsif score == lowest
+            spots += path
+        end
+        next        
+    end
+
+    if path.include?([r, c])
+        next
+    end
+
+    @drdc.each do |ddr, ddc|
+        next if grid[r + ddr, c + ddc] == "#"
+
+        if dr == ddr && dc == ddc
+            pq << [score + 1, r + ddr, c + ddc, ddr, ddc, path + [[r, c]]]
+        elsif (dr + ddr) != 0 || (dc + ddc) != 0
+            pq << [score + 1001, r + ddr, c + ddc, ddr, ddc, path + [[r, c]]]
+        end
+    end
+end
+
+p spots.count + 1
