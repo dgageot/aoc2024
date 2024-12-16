8aefa73b3df3033bda6b97e78a373f65cee8c06a
David Gageot <david.gageot@docker.com>
2024-12-17 00:37:31 +0100

    wip
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 16/second.rb
+++ 16/second.rb
@@ -8,10 +8,10 @@ grid = Grid.new(STDIN.readlines(chomp: true).map { |line| line.chars })
 
 seen = Set.new
 pq = Heap.new { |l, r| l[0] < r[0] }
-pq << [0, grid.height - 2, 1, 0, 1]
+pq << [0, grid.height - 2, 1, 0, 1, []]
 
 loop do
-    score, r, c, dr, dc = pq.pop
+    score, r, c, dr, dc, path = pq.pop
     next if seen.add?([r, c]).nil?
 
     if [r, c] == [1, grid.width - 2]
@@ -23,9 +23,9 @@ loop do
         next if grid[r + ddr, c + ddc] == "#"
 
         if dr == ddr && dc == ddc
-            pq << [score + 1, r + ddr, c + ddc, ddr, ddc]
+            pq << [score + 1, r + ddr, c + ddc, ddr, ddc, path + [[r, c]]]
         elsif (dr + ddr) != 0 || (dc + ddc) != 0
-            pq << [score + 1001, r + ddr, c + ddc, ddr, ddc]
+            pq << [score + 1001, r + ddr, c + ddc, ddr, ddc, path + [[r, c]]]
         end
     end
 end
